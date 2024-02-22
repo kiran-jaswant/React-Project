23c849e702265cdef6db01c50f5da24596184d5b
vthombare242003 <vaishnavithombare2309@gmail.com>
2024-02-22 17:29:06 +0530

    fruits folder added

--- src/index.js
+++ src/index.js
@@ -4,57 +4,13 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import Home from './views/Home/Home';
 import Flowers from './views/Flowers/Flowers';
+import Fruits from "./views/Fruits &Vegetables/Fruits";
 import Decorative from './views/Decorative/Decorative';
 import Gardening from './views/Gardening/Gardening';
 import About from './views/About/About';
 import Contact from './views/Contact/Contact';
 import Login from './views/Login/Login';
 import Register from './views/Register/Register';
-
-
-const router = createBrowserRouter([
-  {
-    path:'/',
-    element:<Home/>
-  },
-  {
-    path:'/flowers',
-    element:<Flowers/>
-  },
-  {
-    path:'/decorative',
-    element:<Decorative/>
-  },
-  {
-    path:'/gardening',
-    element:<Gardening/>
-  },
-  {
-    path:'/about',
-    element:<About/>
-  },
-  {
-    path:'/contact',
-    element:<Contact/>
-  },
-  {
-    path:'/login',
-    element:<Login/>
-  },
-  {
-    path:'/register',
-    element:<Register/>
-  }
-
-]);
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<RouterProvider router={router}/>);
-
-
-=======
-import {createBrowserRouter,RouterProvider} from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -71,6 +27,34 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
     {
         path:"/contact",
         element:<h1>Contact</h1>
+    },
+    {
+      path:'/flowers',
+      element:<Flowers/>
+    },
+    {
+      path:'/decorative',
+      element:<Decorative/>
+    },
+    {
+      path:'/gardening',
+      element:<Gardening/>
+    },
+    {
+      path:'/fruits',
+      element:<Fruits/>
+    },
+    {
+      path:'/login',
+      element:<Login/>
+    },
+    {
+      path:'/register',
+      element:<Register/>
+    },
+    {
+      path:"*",
+      element:<h1>Not Found</h1>
     }
  ]);
 
